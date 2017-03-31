b833a492125ea0e1a46f7377a0145584712e43a2
Roberto Duarte <robert0d3@outlook.com>
2017-03-31 15:12:42 -0600

    Codigo Sensor

---
+++ SENSOR/SENSOR.ino
@@ -0,0 +1,41 @@
+long distancia, tiempo;
+int diodo = 13;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(8, OUTPUT); //Trigger
+  pinMode(9, INPUT); //Echo
+  pinMode(diodo, OUTPUT);
+}
+
+void loop() {
+  //digitalWrite(diodo, HIGH); //Diodo
+  sensor();
+}
+
+void sensor(){
+  digitalWrite(8, LOW);
+  delayMicroseconds(2);
+  digitalWrite(8, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(8, LOW);
+
+  tiempo = pulseIn(9, HIGH);
+  distancia = (tiempo/2)/29;
+
+  if(distancia >= 500 || distancia <= 0){
+    Serial.println("Error");
+  }else{
+    Serial.println(distancia);
+    Serial.println("cm");
+  }
+
+  if(distancia >= 20){
+    digitalWrite(13, HIGH);
+  }else{
+    digitalWrite(13, LOW);
+  }
+
+  delay(500);
+}
+
